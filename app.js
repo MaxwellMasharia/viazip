469288e26d91f0fa70670df9e9d31634e64cd322
MaxwellMasharia <viaxender@gmail.com>
2020-05-02 02:33:21 +0300

    Added zip file creation

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const fileInput = document.querySelector(".file_input");
+const JSZip = require("jszip");
+const saveAs = require("file-saver").saveAs;
+const zip = new JSZip();
 
 fileInput.addEventListener("change", function () {
   const files = fileInput.files;
   console.log(files);
+
+  /* Create a folder containing the zip files */
+  for (const file of files) {
+    const filename = file.name;
+    zip.file(filename, file);
+  }
+
+  console.log("Done");
+
+  /* Download the file */
+  // zip.generateAsync({ type: "blob" }).then(function (blob) {});
+
+  zip
+    .generateAsync({ type: "blob" }, function updateCallback(metadata) {
+      console.log("progression: " + metadata.percent.toFixed(2) + " %");
+      if (metadata.currentFile) {
+        console.log("current file = " + metadata.currentFile);
+      }
+    })
+    .then(function (blob) {
+      saveAs("name.zip",blob);
+    });
 });
